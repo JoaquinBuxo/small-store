5052421beae2834302e11d7655f8c24534f9f3f6
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-10-05 11:14:24 +0200

    init setup

---
+++ data.js
@@ -0,0 +1,12 @@
+export const data = [
+  { img: 'benjerry', price: 5.95, qtty: 0 },
+  { img: 'caffelatte', price: 1.27, qtty: 0 },
+  { img: 'calippo', price: 4.1, qtty: 0 },
+  { img: 'evax', price: 2.4, qtty: 0 },
+  { img: 'pizza', price: 4.95, qtty: 0 },
+  { img: 'scottex', price: 4.5, qtty: 0 },
+  { img: 'spaghetti', price: 1.25, qtty: 0 },
+  { img: 'triangulos', price: 2.35, qtty: 0 },
+  { img: 'xibeca', price: 3.75, qtty: 0 },
+  { img: 'chipsahoy', price: 2.2, qtty: 0 },
+];
